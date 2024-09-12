ea3149e2f80df2c3e08fa46e595be288adc79d99
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-09-13 02:43:53 +0500

    Update getUserData.js

--- server/controllers/getUserData.js
+++ server/controllers/getUserData.js
@@ -11,25 +11,13 @@ export const getUserData = async (req, res) => {
       avatar: user.avatar,
       links: user.links,
     };
+    const socials = user.socialMedia;
     return res.json({
       message: "found",
       userData,
+      socials,
       status: "success",
     });
-  } catch (error) {
-    console.log(error);
-    return res.json({ status: "error", error: error.message });
-  }
-};
-
-export const getUserSocials = async (req, res) => {
-  const handle = req.params.handle;
-  try {
-    console.log("Handle:", handle);
-    const user = await User.findOne({ handle: handle });
-    const socials = user.socialMedia;
-    console.log(socials);
-    return res.json({ message: "found", socials, status: "success" });
   } catch (error) {
     console.log(error);
     return res.json({ status: "error", error: error.message });
