7b22d2fac7c5fbdb89ea1bc721bd485870f910eb
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-09-13 01:05:36 +0500

    Update Header.js

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,18 +1,41 @@
 import Image from "next/image";
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { useRouter } from "next/router";
-import { toast } from "react-toastify";
-// import { ToolTip } from "./ToolTip";
 import { Tooltip } from "@geist-ui/core";
+import Link from "next/link";
+import UserContext from "@/context/userContext";
 
-const Header = ({ data }) => {
+const Header = () => {
   const router = useRouter();
-  // console.log(data);
-  const { name, role, avatar, handle, links } = data;
   const handleLogout = () => {
     localStorage.removeItem("LinkTreeToken");
     router.push("/login");
   };
+
+  const { userData, setUserData } = useContext(UserContext);
+  const { role, avatar, handle } = userData;
+
+  useEffect(() => {
+    if (!localStorage.getItem("LinkTreeToken")) router.push("/");
+
+    fetch("http://localhost:8080/data/dashboard", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        tokenMail: localStorage.getItem("LinkTreeToken"),
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === "error") return toast.error("Error");
+        setData(data.userData);
+        setUserData(data.userData);
+        localStorage.setItem("userHandle", data.userData.handle);
+        toast.success(data.message);
+        console.log(data.message);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <header className="flex flex-row justify-between items-start">
@@ -24,7 +47,10 @@ const Header = ({ data }) => {
           leaveDelay={0}
           hideArrow
         >
-          <button className="inline-flex w-auto px-3 py-3 mb-3 border rounded-md border-purple-500 text-purple-500 font-bold hover:text-purple-800 hover:bg-purple-100">
+          <Link
+            href={"/edit/links"}
+            className="inline-flex w-auto px-3 py-3 mb-3 border rounded-md border-purple-500 text-purple-500 font-bold hover:text-purple-800 hover:bg-purple-100"
+          >
             <Image
               src={"/svg/edit.svg"}
               width={24}
@@ -32,7 +58,7 @@ const Header = ({ data }) => {
               alt="Edit Links"
             />
             <span className="hidden md:block ml-2">Edit Links</span>
-          </button>
+          </Link>
         </Tooltip>
         <Tooltip
           text={"Edit Profile"}
@@ -41,7 +67,10 @@ const Header = ({ data }) => {
           leaveDelay={0}
           hideArrow
         >
-          <button className="inline-flex w-auto px-3 py-3 mb-3 border rounded-md border-red-500 text-red-500 font-bold hover:text-red-800 hover:bg-red-100">
+          <Link
+            href={"/edit/profile"}
+            className="inline-flex w-auto px-3 py-3 mb-3 border rounded-md border-red-500 text-red-500 font-bold hover:text-red-800 hover:bg-red-100"
+          >
             <Image
               src={"/svg/avatar.svg"}
               width={24}
@@ -49,7 +78,7 @@ const Header = ({ data }) => {
               alt="Edit Profile"
             />
             <span className="hidden md:block ml-2">Edit Profile</span>
-          </button>
+          </Link>
         </Tooltip>
       </div>
       <div className="flex flex-row gap-2 ps-0 p-3">
@@ -60,7 +89,7 @@ const Header = ({ data }) => {
           leaveDelay={0}
           hideArrow
         >
-          <a href={`/${handle}`}>
+          <Link href={`/${handle}`}>
             <div className="inline-flex items-center w-auto px-3 py-2 mb-3 border rounded-md bg-gray-200 hover:bg-gray-300">
               <div className="flex flex-col text-xs text-right mr-2">
                 <span className="font-bold">{handle || "Mr. AnOob"}</span>
@@ -70,7 +99,7 @@ const Header = ({ data }) => {
                 <img src={avatar} width={32} height={32} alt={handle} />
               </div>
             </div>
-          </a>
+          </Link>
         </Tooltip>
         <div className="inline-flex justify-end gap-2">
           <Tooltip
