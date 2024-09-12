74bcb4c594187d7ff4215e5c3390ec3f7929622b
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-09-13 00:12:58 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import connectToDB from "./db.js";
 import "dotenv/config";
 import { loginUser, registerUser } from "./controllers/auth.js";
 import { dashBoardData } from "./controllers/dashboard.js";
-import { getUserData, getUserSocials } from "./controllers/getUserData.js";
+import { getUserData } from "./controllers/getUserData.js";
 
 const app = express();
 connectToDB();
@@ -20,9 +20,9 @@ app.get("/", (req, res) => {
 // Routes
 app.post("/api/register", registerUser);
 app.post("/api/login", loginUser);
+
 app.post("/data/dashboard", dashBoardData);
 app.get("/get/:handle", getUserData);
-app.get("/get/socials/:handle", getUserSocials);
 
 const port = process.env.PORT || 8080;
 
