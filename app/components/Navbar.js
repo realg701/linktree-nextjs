7dab0b13060ef275c2544897094a22d6e40e099f
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-09-14 00:48:24 +0500

    Update Navbar.js

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -6,6 +6,7 @@ const NavBar = () => {
   const router = useRouter();
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const [open, setOpen] = useState(false);
+  const [handle, setHandle] = useState("");
 
   const toggleMobileMenu = () => {
     setMobileMenuOpen(!mobileMenuOpen);
@@ -13,6 +14,8 @@ const NavBar = () => {
   useEffect(() => {
     setMobileMenuOpen(false);
     if (localStorage.getItem("LinkTreeToken")) setOpen(true);
+    if (localStorage.getItem("userHandle"))
+      setHandle(localStorage.getItem("userHandle"));
   }, [router.asPath]);
 
   return (
@@ -70,10 +73,10 @@ const NavBar = () => {
               </li>
               <li>
                 <Link
-                  href="/features"
+                  href={`/${handle}`}
                   className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
-                  Features
+                  {handle.toUpperCase()}
                 </Link>
               </li>
               {open ? (
