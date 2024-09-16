03b5b92200170c15722ce687bf9000d3ffbbf642
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-09-17 04:39:07 +0500

    Update _app.js

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,27 +1,30 @@
-import { useState, useEffect } from "react";
+import React from "react";
 import { useRouter } from "next/router";
 import Script from "next/script";
+import NProgress from "nprogress";
 import { ToastContainer } from "react-toastify";
 import UserContext from "@/context/userContext";
 import Footer from "../components/Footer";
 import NavBar from "../components/Navbar";
-import NProgress from "nprogress";
+import NetworkStatusProvider from "@/components/NetworkStatusProvider";
+import Loader from "@/components/Loader";
 import "../styles/globals.css";
 import "../public/nprogress.css";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
-  const [isLoading, setIsLoading] = useState(false);
-  const [userData, setUserData] = useState({
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [userData, setUserData] = React.useState({
     name: "",
     role: "",
     bio: "",
     avatar: "",
     handle: "",
+    socials: "",
   });
 
-  useEffect(() => {
+  React.useEffect(() => {
     const handleStart = () => {
       setIsLoading(true);
       NProgress.start();
@@ -44,13 +47,13 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
-      <NavBar />
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-WYTYXQXVK6`}
-      />
-      <Script strategy="lazyOnload">
-        {`
+      <NetworkStatusProvider>
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-WYTYXQXVK6`}
+        />
+        <Script strategy="lazyOnload">
+          {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
@@ -58,17 +61,23 @@ export default function App({ Component, pageProps }) {
                     page_path: window.location.pathname,
                     });
                 `}
-      </Script>
-      <UserContext.Provider value={{ userData, setUserData }}>
-        <Component {...pageProps} />
-      </UserContext.Provider>
-      <ToastContainer position="bottom-left" />
-      {isLoading && (
-        <div className="nprogress-custom-parent">
-          <div className="nprogress-custom-bar" />
-        </div>
-      )}
-      <Footer />
+        </Script>
+        <UserContext.Provider value={{ userData, setUserData }}>
+          <NavBar />
+          {isLoading ? (
+            <>
+              <Loader loaderState="Loading..." />
+              <div className="nprogress-custom-parent">
+                <div className="nprogress-custom-bar" />
+              </div>
+            </>
+          ) : (
+            <Component {...pageProps} />
+          )}
+          {!userData.handle && <Footer />}
+        </UserContext.Provider>
+        <ToastContainer position="bottom-left" />
+      </NetworkStatusProvider>
     </>
   );
 }
