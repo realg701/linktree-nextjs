7009bf85e21a4ff3d2e80d43cb89eb9e51ff6063
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-09-19 02:12:53 +0500

    Update LinkTree.js

--- app/components/LinkTree.js
+++ app/components/LinkTree.js
@@ -8,21 +8,15 @@ const LinkTree = ({ data, handle }) => {
 
   return (
     <>
-      <section className="relative max-w-3xl md:w-2/3 my-0 mx-auto">
+      <section className="relative max-w-3xl md:w-2/3 my-0 mx-auto text-center">
         <img
           className="absolute w-32 h-32 mt-6 rounded-full left-1/2 -translate-x-1/2"
           src={avatar}
           alt="Avatar"
         />
-        <h2 className="pt-40 text-center text-lg font-bold">
-          {name || handle}
-        </h2>
-        {name && (
-          <h2 className="pb-2 text-center font-medium text-gray-500">
-            @{handle}
-          </h2>
-        )}
-        <p className="text-center px-2">{bio}</p>
+        <h2 className="pt-40 text-lg font-bold">{name || handle}</h2>
+        {name && <h2 className="pb-2 font-medium text-gray-500">@{handle}</h2>}
+        <p className="px-2">{bio}</p>
         <div className="p-5">
           <AnimatePresence>
             {links.map((link, index) => (
