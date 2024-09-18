8de571ccfadc1f4bcb5eff0a1534d1874e2f37d6
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-09-19 01:23:47 +0500

    Update dashboard.js

--- app/pages/dashboard.js
+++ app/pages/dashboard.js
@@ -1,37 +1,12 @@
-import React, { useContext, useEffect, useState } from "react";
+import React from "react";
 import MyHead from "@/components/MyHead";
 import LinkBox from "@/components/LinkBox";
 import Header from "@/components/Header";
-import { useRouter } from "next/router";
-import { toast } from "react-toastify";
 import UserContext from "@/context/userContext";
 
 const dashboard = () => {
-  const router = useRouter();
-  const [data, setData] = useState({});
-  const { setUserData } = useContext(UserContext);
+  const { userData } = React.useContext(UserContext);
 
-  useEffect(() => {
-    if (!localStorage.getItem("LinkTreeToken")) return router.push("/");
-
-    fetch("http://localhost:8080/data/dashboard", {
-      method: "POST",
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify({
-        tokenMail: localStorage.getItem("LinkTreeToken"),
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.status === "error") return toast.error("Error");
-        setData(data.userData);
-        setUserData(data.userData);
-        localStorage.setItem("userHandle", data.userData.handle);
-        toast.success(data.message);
-        console.log(data.message);
-      })
-      .catch((error) => console.log(error));
-  }, []);
   return (
     <>
       <MyHead
@@ -46,7 +21,7 @@ const dashboard = () => {
         <section className="grid md:grid-cols-2 xl:grid-cols-4 gap-5 cursor-default">
           <LinkBox
             lbTitle="Links"
-            lbNumber={data.links}
+            lbNumber={userData.links?.length}
             lbSvg="url"
             lbTheme="bg-red-500"
             lbAlt="Links"
